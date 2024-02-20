a22174485726a97d90212caea332ceb9f89da46c
Yusri <muhd.yusri@rhbgroup.com>
2024-02-20 23:11:45 +0800

    initial

---
+++ lib/main.dart
@@ -0,0 +1,63 @@
+import 'dart:io';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:ku_didik/home_page.dart';
+import 'package:ku_didik/login_page.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  Platform.isAndroid
+      ? await Firebase.initializeApp(
+          options: FirebaseOptions(
+              apiKey: "AIzaSyBxHV4XrCiO-4vwU8flCtvmyJQvlT78ZZQ",
+              appId: "1:783471223137:android:0fff503a7f2c2677eb2520",
+              messagingSenderId: "783471223137",
+              projectId: "didik2ku"))
+      : await Firebase.initializeApp();
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color.fromARGB(255, 58, 177, 183)),
+        useMaterial3: false,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text('Firebase Login'),
+        ),
+        body: StreamBuilder<User?>(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            } else if (snapshot.hasError) {
+              return const HomePage();
+            } else if (snapshot.hasData) {
+              return const Center(
+                child: Text('User is logged in'),
+              );
+            } else {
+              return const LoginPage();
+            }
+          },
+        ),
+      ),
+    );
+  }
+}
